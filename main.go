876ecbb9bf6e4eaca59cf5ebe3547cd4793edc9b
Fillonit Ibishi <filloniti@gmail.com>
2023-08-29 23:57:57 +0200

    update

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ func main() {
 	router.SetTrustedProxies([]string{"127.0.0.1"})
 	configs.ConnectDB()
 
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"status": "active",
+			"author": "https://github.com/Fillonit",
+			"github": "https://github.com/Fillonit/MicroGo",
+		})
+	})
+
 	routes.UserRoute(router)
 	routes.PostRoute(router)
 
