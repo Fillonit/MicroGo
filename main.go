a9d597d3f7be7d45522779aecda66de22d5f18f8
Fillonit Ibishi <filloniti@gmail.com>
2023-08-29 14:39:13 +0200

    Initial Commit ✨

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"MicroGo/configs"
+	"MicroGo/routes"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.SetTrustedProxies([]string{"127.0.0.1"})
+	configs.ConnectDB()
+
+	routes.UserRoute(router)
+
+	router.Run("localhost:8080")
+}
